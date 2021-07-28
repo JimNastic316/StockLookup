3707324510897b46fd7686d76ac09036903bbcdc
JimNastic316 <JimNastic316@gmail.com>
2021-07-28 12:16:08 -0400

    Finished
    all done

--- stock_quote.py
+++ stock_quote.py
@@ -1,9 +1,35 @@
 import requests
-import time
+# import time
 
 ticker = "MSFT"
 api_key = "146a407827e04b31afc509d21a7fe387"
 
-def get_stpcl_price(ticker_symbol, api):
+
+def get_stock_price(ticker_symbol, api):
     url = f"https://api.twelvedata.com/price?symbol={ticker_symbol}&apikey={api}"
     response = requests.get(url).json()
+    price = response['price'][:-3]
+    return price
+
+
+def get_stock_quote(ticker_symbol, api):
+    url = f"https://api.twelvedata.com/quote?symbol={ticker_symbol}&apikey={api}"
+    response = requests.get(url).json()
+    return response
+
+
+stockdata = get_stock_quote(ticker, api_key)
+stock_price = "$" + get_stock_price(ticker, api_key)
+
+name = stockdata['name']
+exchange = stockdata['exchange']
+currency = stockdata['currency']
+open_price = stockdata['open']
+high_price = stockdata['high']
+low_price = stockdata['low']
+close_price = stockdata['close']
+volume = stockdata['volume']
+
+
+
+print(name, stock_price)
